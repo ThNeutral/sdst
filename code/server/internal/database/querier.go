df77da31263a89fa1ef3a97b11c847c51f92063d
Sil1con <143268089+Sil1con@users.noreply.github.com>
2024-12-09 21:37:14 +0200

    Roles and Add/Delete functionalities

--- code/server/internal/database/querier.go
+++ code/server/internal/database/querier.go
@@ -11,16 +11,20 @@ import (
 )
 
 type Querier interface {
+	AddUserToProject(ctx context.Context, arg AddUserToProjectParams) error
 	CreateMessage(ctx context.Context, arg CreateMessageParams) (Message, error)
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
 	DeleteUserByToken(ctx context.Context, token uuid.UUID) error
 	DeleteUserByUsername(ctx context.Context, username string) error
+	DeleteUserFromProject(ctx context.Context, arg DeleteUserFromProjectParams) error
 	GetMessagesByProject(ctx context.Context, projectID uuid.UUID) ([]Message, error)
 	GetMessagesByUsers(ctx context.Context, userID uuid.UUID) ([]Message, error)
 	GetUserByEmail(ctx context.Context, email string) (User, error)
+	GetUserById(ctx context.Context, arg GetUserByIdParams) (ProjectUser, error)
 	GetUserByToken(ctx context.Context, token uuid.UUID) (User, error)
 	GetUserByUsername(ctx context.Context, username string) (User, error)
 	UpdateLastLoginTime(ctx context.Context, arg UpdateLastLoginTimeParams) error
+	UpdateUserRole(ctx context.Context, arg UpdateUserRoleParams) error
 }
 
 var _ Querier = (*Queries)(nil)
